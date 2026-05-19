8ede9cac7a4b6efd846ddf0f2486815dfb979697
JeetDas5 <jeet15083011@gmail.com>
2026-05-20 00:01:59 +0530

    feat: add rate limiter

--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,13 @@
-import express from "express";
 import cors from "cors";
+import helmet from "helmet";
+import express from "express";
 
 import authRoutes from "./routes/auth.route";
 
 const app = express();
 
 app.use(cors());
-
+app.use(helmet());
 app.use(express.json());
 
 app.get("/", (req, res) => {
---
+++ server/middlewares/rateLimiter.ts
@@ -0,0 +1,15 @@
+import rateLimit from "express-rate-limit";
+
+export const authLimiter = rateLimit({
+  windowMs: 60 * 1000,
+
+  max: 10,
+
+  message: {
+    message: "Too many requests. Please try again later.",
+  },
+
+  standardHeaders: true,
+
+  legacyHeaders: false,
+});
